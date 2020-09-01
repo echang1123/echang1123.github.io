f91ef57e8156a73e2fdfe44e34b1ca020c95a9f5
echang1123 <exc8593@rit.edu>
2020-08-31 22:52:08 -0400

    updated the portfolio and other text

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$( document ).ready( function() {
+  // Add smooth scrolling to menu tabs
+    $( "a" ).on( 'click', function( event ) {
+        if ( this.hash !== "" ) {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+            var scrollSpeed = 400;
+            
+            $( 'html, body' ).animate( { 
+                scrollTop: $( hash ).offset().top
+            }, scrollSpeed, function() {
+                
+            window.location.hash = hash;
+            } );
+        }
+    } );
+    
+    //for sticky navbar
+    window.onscroll = function() { myFunction() };
+    var navbar = document.getElementById( 'navbar' );
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if ( window.pageYOffset >= sticky ) {
+        navbar.classList.add( "sticky" )
+      } else {
+        navbar.classList.remove( "sticky" );
+      }
+    }
+    
+    //for highlighting navbar tab to show which page you're on
+    var header = document.getElementById( "navbuttons" );
+    var btns = header.getElementsByClassName( "btn" );
+    for ( var i = 0; i < btns.length; i++ ) {
+        btns[i].addEventListener( "click", function() {
+        var current = document.getElementsByClassName( "active" );
+        current[0].className = current[0].className.replace( " active", "" );
+        this.className += " active";
+        } );
+    }
+    
+} );
